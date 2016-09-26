93ee55b81f8d4b6435a6b28ac292c560686e891c
buckwheatthedog <nsteigmann@cca.edu>
2016-09-25 19:53:07 -0700

    Create Problem3.js
    I was still having problems with the actual P5.js program not running. I checked the syntax and rechecked the logic. Unfortunately I don't know the developer tools to fix the problem with the Chrome browser on my own so have no way of testing this. It's sort of a problem, and after going into console, it only displays the problem 1/4 of the time.  If there are any alternatives to p5, or a solid get around for this happening all the time, it would be very useful to know. Thank you.

---
+++ Problem3.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500, 500);
+    background(255);
+
+  for (var i = 5; i < 500; i = i + 5) {
+    for (var j = 10; j < 500; j = i * 2) {
+
+      startX = i;
+      startY = 0;
+      endX = i; 
+      endY = j;
+
+      line(startX, startY, endX, endY);
+
+
+    }
+  }
+
+}
