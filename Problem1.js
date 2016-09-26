1b3572c0bb6ad9e8b63e134c28da035b038bda9f
buckwheatthedog <nsteigmann@cca.edu>
2016-09-25 19:19:46 -0700

    Create Problem1.js

---
+++ Problem1.js
@@ -0,0 +1,15 @@
+function setup() {
+  background(255);
+  createCanvas(500, 500);
+
+  for (var i = 0; i < 500; i = i + 25) {
+    var startX = 250;
+    var startY = 0;
+    var endX = i;
+    var endY = 500;
+
+    line(startX, startY, endX, endY);
+
+
+  }
+}
