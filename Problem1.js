603d3b9e92e24fd106c2930084c5e30fb889f22a
buckwheatthedog <nsteigmann@cca.edu>
2016-09-28 08:08:55 -0700

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -2,7 +2,7 @@ function setup() {
   background(255);
   createCanvas(500, 500);
 
-  for (var i = 0; i < 500; i = i + 25) {
+  for (var i = 0; i < 500; i = i + 10) {
     var startX = 250;
     var startY = 0;
     var endX = i;
