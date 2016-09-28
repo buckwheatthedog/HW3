05a8dacf9b75c91a9ad859ab6ae47dc3d634ecc5
buckwheatthedog <nsteigmann@cca.edu>
2016-09-28 08:20:55 -0700

    Update Problem3.js

--- Problem3.js
+++ Problem3.js
@@ -1,19 +1,17 @@
 function setup() {
   createCanvas(500, 500);
-    background(255);
+  background(255);
 
   for (var i = 5; i < 500; i = i + 5) {
-    for (var j = 10; j < 500; j = i * 2) {
+//    for (var j = 10; j < 500; j = i * 2) {
 
       startX = i;
       startY = 0;
-      endX = i; 
-      endY = j;
+      endX = i;
+      endY = i^2;
 
       line(startX, startY, endX, endY);
 
 
     }
   }
-
-}
